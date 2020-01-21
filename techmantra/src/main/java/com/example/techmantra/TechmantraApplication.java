8bfaba8a314165b9e8ac20ce987a10b1bc0aa5a7
arojit007 <arojit@thirdeyedata.io>
2020-01-21 11:46:42 +0530

    Spring Initialyzr

---
+++ techmantra/src/main/java/com/example/techmantra/TechmantraApplication.java
@@ -0,0 +1,13 @@
+package com.example.techmantra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechmantraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechmantraApplication.class, args);
+	}
+
+}
